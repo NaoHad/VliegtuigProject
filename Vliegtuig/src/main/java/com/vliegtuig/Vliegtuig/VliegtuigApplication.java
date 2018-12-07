88549f2898ccd8540a4e5aeefccbc2965f86f452
NaoHad <naoufalhaddou@gmail.com>
2018-12-07 15:24:17 +0100

    nieuw

---
+++ Vliegtuig/src/main/java/com/vliegtuig/Vliegtuig/VliegtuigApplication.java
@@ -0,0 +1,12 @@
+package com.vliegtuig.Vliegtuig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VliegtuigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VliegtuigApplication.class, args);
+	}
+}
